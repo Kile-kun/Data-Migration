6fb4c4d69fb623825645d0790f5659e928129f4c
Kile_kun <97529068+Kile-kun@users.noreply.github.com>
2023-03-28 16:42:47 +0100

    Add files via upload

--- DM_db.sql
+++ DM_db.sql
@@ -3,25 +3,47 @@ CREATE DATABASE dm_db;
 USE dm_db;
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer (
-			RowNumber		INT,
 			CustomerId		INT,
 			Surname			VARCHAR(256),
+            Gender			ENUM ('Male', 'Female'),
+            Age				INT,
+            IsActiveMember	ENUM ('1', '0'),
+            Tenure			INT,
+            HasCrCard		ENUM ('1', '0'),
 			CreditScore		INT,
-			Geography		VARCHAR(256),
-			Gender			ENUM ('Male', 'Female'),
-			Age				INT,
-			Tenure			INT,
-			Balance			DECIMAL(10,2),
-			NumOfProducts	INT,
-			HasCrCard		ENUM ('1', '0'),
-			IsActiveMember	ENUM ('1', '0'),
-			EstimatedSalary	DECIMAL(10,2),
 	PRIMARY KEY (customerId)
+);
+CREATE TABLE Geography(
+			GeoId			VARCHAR(256),
+            Country			VARCHAR(256),
+	PRIMARY KEY (GeoId)
+);
+CREATE TABLE Transaction(
+			TransactionId	INT,
+			CustomerId		INT,
+            GeoId			VARCHAR(256),
+			NumOfProducts	INT,
+            Balance 		DECIMAL(10,2),
+	FOREIGN KEY (CustomerId) REFERENCES Customer(CustomerId),
+    FOREIGN KEY (GeoId) REFERENCES Geography (GeoId),
+	PRIMARY KEY (TransactionId)
 );
 USE dm_db;
 SET GLOBAL LOCAL_INFILE=true;
-LOAD DATA LOCAL INFILE 'C:/Users/babat/OneDrive/Documents/Tech1M DA Bootcamp/Project/Data Migration Project/data_migration.csv'
+LOAD DATA LOCAL INFILE 'C:/Users/babat/OneDrive/Documents/Tech1M DA Bootcamp/Project/Data Migration Project/Customer.csv'
 INTO TABLE customer
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES
+TERMINATED BY '\n'
+IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:/Users/babat/OneDrive/Documents/Tech1M DA Bootcamp/Project/Data Migration Project/Geography.csv'
+INTO TABLE geography
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"' LINES
+TERMINATED BY '\n'
+IGNORE 1 ROWS;
+LOAD DATA LOCAL INFILE 'C:/Users/babat/OneDrive/Documents/Tech1M DA Bootcamp/Project/Data Migration Project/Transaction.csv'
+INTO TABLE transaction
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"' LINES
 TERMINATED BY '\n'
